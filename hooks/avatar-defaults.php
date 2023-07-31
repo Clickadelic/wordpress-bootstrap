21df0642458c4b89fb9166d2c6a86d46656a0464
Clickadelic <toby.hopp@gmail.com>
2023-08-01 01:35:40 +0200

    theme.json test

--- hooks/avatar-defaults.php
+++ hooks/avatar-defaults.php
@@ -1,9 +1,9 @@
 <?php
 
-function theme_custom_avatar ($avatar_defaults) {
+function bootstrap_custom_avatar ($avatar_defaults) {
 	$myavatar =  get_template_directory_uri().'/assets/images/Default-Avatar.png';
 	$new_avatar[$myavatar] = __('Bootstrap default avatar', 'bootstrap');
 	$avatar = array_merge($new_avatar, $avatar_defaults);
 	return $avatar;
 }
-add_filter('avatar_defaults', 'theme_custom_avatar');
+add_filter('avatar_defaults', 'bootstrap_custom_avatar');
